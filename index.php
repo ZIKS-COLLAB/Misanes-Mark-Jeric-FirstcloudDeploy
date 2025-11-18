c3be6dd9387c5da032d1fa27af636cd12f90d448
ZIKS-COLLAB <jericmisanes792@gmail.com>
2025-11-18 12:27:36 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+
+<!DOCTYPE html>
+<html>
+  <body>
+    <h1>My First Cloud Deployment</h1>
+    <h2>Hello, my name is Mark Jeric Misanes </h2>
+    <img src="download (6).jpeg" width="200" />
+    <p>This is my first deployed website!</p>
+  </body>
+</html>
